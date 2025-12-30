12693b90472726de2c976359242b60a98e35539d
Suraj Kumar <surajgautam56878@gmail.com>
2025-12-30 19:10:59 +0530

    add:configure style file of auth, home, create and color scheme with utils

---
+++ mobile/lib/utils.js
@@ -0,0 +1,9 @@
+export function formatDate(dateString) {
+  // format date nicely
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
